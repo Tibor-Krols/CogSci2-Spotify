0b41301cd0d0cb255622a26d02d28ff25d992eb5
yananikol <yana.nik98@gmail.com>
2022-05-29 19:55:33 +0200

    updating lyrics stuff

--- lyrics_features.py
+++ lyrics_features.py
@@ -12,18 +12,21 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR 
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score, GridSearchCV
+
 
 class LyricsTable:
 
-    def __init__(self, regressor, testset = None, params = None):
+    def __init__(self, regressor, testset = None):
         self.regressor = regressor
         self.testset = testset
-        self.params = params 
 
-        self.features_train = (pd.read_csv('lyrics_features_train.csv', delimiter = ',').to_numpy()).astype(np.float32)
-        self.features_test = (pd.read_csv('lyrics_features_test.csv', delimiter = ',').to_numpy()).astype(np.float32)
-        self.features_val = (pd.read_csv('lyrics_features_val.csv', delimiter = ',').to_numpy()).astype(np.float32)
-        self.labels_train_v = pd.read_csv('merged_cleaned_sentiment_train.csv', delimiter = ',').dropna(axis = 0)['y_valence']
+        self.features_train = pd.read_csv('lyrics_features_train.csv', delimiter = ',').values
+        print(self.features_train.shape)
+        self.features_test = pd.read_csv('lyrics_features_test.csv', delimiter = ',').values
+        self.features_val = pd.read_csv('lyrics_features_val.csv', delimiter = ',').values
+        self.labels_train_v = pd.read_csv('merged_cleaned_sentiment_train.csv', delimiter = ',').dropna(axis = 0)['y_valence'].values
+        print(self.labels_train_v.shape)
         self.labels_train_a = pd.read_csv('merged_cleaned_sentiment_train.csv', delimiter = ',').dropna(axis = 0)['y_arousal']
         self.labels_test_v = pd.read_csv('merged_cleaned_sentiment_test.csv', delimiter = ',').dropna(axis = 0)['y_valence']
         self.labels_test_a = pd.read_csv('merged_cleaned_sentiment_test.csv', delimiter = ',').dropna(axis = 0)['y_arousal']
@@ -49,13 +52,13 @@ class LyricsTable:
 
         self.generate_table(self.results)
         
-    def init_regr(self, params = None):
+    def init_regr(self):
         if self.regressor == 'linreg':
-            return LinearRegression(positive = params)
+            return LinearRegression(positive = True, fit_intercept= True)
         elif self.regressor == 'rf':
-            return RandomForestRegressor(n_estimators= params[0], max_depth= params[1])
+            return RandomForestRegressor()
         elif self.regressor == 'mlp':
-            return MLPRegressor(params)
+            return MLPRegressor()
         elif self.regressor == 'svr':
             return SVR()
 
@@ -81,12 +84,12 @@ class LyricsTable:
         return res_array[:, 2:]
 
     def regression_r2(self, train, test, lab_train_a, lab_train_v, lab_a, lab_v):
-        regr_val = self.init_regr(self.params)
+        regr_val = self.init_regr()
         regr_val.fit(train, lab_train_v)
         self.prediction_val = regr_val.predict(test)
         r2_val = r2_score(lab_v, self.prediction_val)
 
-        regr_ar = self.init_regr(self.params)
+        regr_ar = self.init_regr()
         regr_ar.fit(train, lab_train_a)
         self.prediction_ar = regr_ar.predict(test)
         r2_ar = r2_score(lab_a, self.prediction_ar)
@@ -99,43 +102,8 @@ class LyricsTable:
         for key in results.keys():
             print(f"{key}\t\t{results[key][0]}\t{results[key][1]}")
 
-l = LyricsTable(regressor='linreg', testset='val',params = [True])
+l = LyricsTable(regressor='linreg', testset='val')
 l.create_table()
-
-### HYPERPARAMETER OPTIMIZATION: RANDOM FOREST
-
-"""params = {'max_depth':[10, 15, 20], 'n_estimators':[50,100]}
-
-for i in params['n_estimators']:
-    for j in params['max_depth']:
-        m = LyricsTable(regressor='rf', testset='val', params = [i,j]) 
-        kf = KFold(n_splits = 5)
-
-        r2s = []
-
-        for train_index, test_index in kf.split(m.features_train):
-            X_train, X_test = m.features_train[train_index], m.features_train[test_index]
-            y_train_v, y_test_v = m.labels_train_v[train_index], m.labels_train_v[test_index]
-            y_train_a, y_test_a = m.labels_train_a[train_index], m.labels_train_a[test_index]
-
-            r2_v, r2_a = m.regression_r2(X_train, X_test, y_train_a, y_train_v, y_test_a, y_test_v)
-            r2s.append(np.array([r2_v, r2_a]))
-
-        print(f'Estimator:{i}, Depth:{j}, R2 = {np.sum(r2s, axis = 0)/5}')"""
-
-"""
-Results:
-Estimator:10, Depth:2, R2 = [ 0.00172388 -0.00253295]
-Estimator:10, Depth:5, R2 = [-0.00969125 -0.01562575]
-Estimator:10, Depth:10, R2 = [-0.04541811 -0.04255748]
-Estimator:10, Depth:15, R2 = [-0.08558572 -0.08581147]
-Estimator:10, Depth:20, R2 = [-0.09596518 -0.1100134 ]
-Estimator:50, Depth:2, R2 = [ 0.00227057 -0.00204339]
-Estimator:50, Depth:5, R2 = [ 0.00140073 -0.00258205]
-"""
-
-
-
 
     
 
